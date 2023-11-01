6faf80cbd22d3516332168a10235edc71bed05a1
Mark McCaskey <maoeurk@gmail.com>
2023-11-01 21:04:05 +0800

    Actually make title screen load the game

--- Levels/title_screen_background.gd
+++ Levels/title_screen_background.gd
@@ -7,3 +7,7 @@ func _ready() -> void:
 
 func _on_start_button_pressed() -> void:
 	animation_player.play("CameraFly")
+
+func _load_first_level() -> void:
+	var first_level = load("res://main.tscn")
+	get_tree().change_scene_to_packed(first_level)
