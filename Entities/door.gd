cbd8d1a74fc8e5945ffbb399c3c2fd139d23e559
Mark McCaskey <maoeurk@gmail.com>
2023-11-01 22:15:13 +0800

    Add export var to flip door open direction

--- Entities/door.gd
+++ Entities/door.gd
@@ -5,12 +5,12 @@ class_name Door extends Node3D
 	get: return closed
 	set(val):
 		closed = val
-		if closed:
-			t = 0
-			dt = -1
-		else:
-			t = 1
-			dt = 1
+		_set_t_dt()
+@export var flip_direction: bool = false:
+	get: return flip_direction
+	set(val):
+		flip_direction = val
+		_set_t_dt()
 enum DoorType { Inner, Outer }
 @export var door_type: DoorType
 
@@ -20,13 +20,27 @@ enum DoorType { Inner, Outer }
 var t: float = 0 # 0 = closed, 1 = opened
 var dt: float = -1 # -1 = closing, +1 = opening
 
+func _set_t_dt() -> void:
+	if flip_direction: dt = 1
+	else: dt = -1
+	if closed:
+		t = 0
+	else:
+		if flip_direction: t = -1
+		else: t = 1
+		dt *= -1
+
 func _ready() -> void:
 	_load_door_mesh()
 	animation_tree.active = true
 
 func _process(delta):
-	t += delta * dt
-	t = clampf(t, 0., 1.)
+	if flip_direction:
+		t += delta * dt
+		t = clampf(t, -1., 0.)
+	else:
+		t += delta * dt
+		t = clampf(t, 0., 1.)
 	animation_tree["parameters/Open/blend_position"] = t
 
 func _load_door_mesh() -> void:
