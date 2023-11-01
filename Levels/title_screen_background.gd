e9221837cb4d02243e0e2b55887804c250490941
Mark McCaskey <maoeurk@gmail.com>
2023-11-01 22:28:44 +0800

    Polish up title screen a bit

--- Levels/title_screen_background.gd
+++ Levels/title_screen_background.gd
@@ -5,9 +5,17 @@ extends Node3D
 func _ready() -> void:
 	animation_player.play("Idle")
 
+func _unhandled_input(event: InputEvent) -> void:
+	if event.is_action_pressed("start"):
+		animation_player.play("CameraFly")
+
 func _on_start_button_pressed() -> void:
 	animation_player.play("CameraFly")
 
 func _load_first_level() -> void:
 	var first_level = load("res://main.tscn")
 	get_tree().change_scene_to_packed(first_level)
+
+func _on_quit_button_pressed() -> void:
+	# TODO: confirm prompt
+	get_tree().quit()
