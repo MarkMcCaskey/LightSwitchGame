15a28025022f2bcc9e200412f05bcf587eed4aab
Mark McCaskey <maoeurk@gmail.com>
2023-11-04 22:03:30 +0800

    Add sliding glass door on back porch

--- Entities/sliding_glass_door_piece.gd
+++ Entities/sliding_glass_door_piece.gd
@@ -1,1 +1,17 @@
 class_name SlidingGlassDoorPiece extends Node3D
+
+@export var is_open: bool = false
+@onready var animation_player: AnimationPlayer = $AnimationPlayer
+
+func _ready() -> void:
+	if is_open:
+		animation_player.play("Open")
+	else:
+		animation_player.play("Closed")
+
+func _on_interactable_on_interact() -> void:
+	is_open = !is_open
+	if is_open:
+		animation_player.play("ClosedToOpen")
+	else:
+		animation_player.play("OpenToClosed")
